3a59c6b42311df6af0db5ce4fe01765f5a3853e3
MoreiraGabryel <marcosgabryel25@gmail.com>
2026-03-17 14:52:35 -0300

    Adicionando outros Métodos Simples, Todas requisições feita usando o PostMan, Criação de um método findByNome no produtoRepository para usar como um método no ProdutoController

--- produtosapi/src/main/java/com/Moreira/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/Moreira/produtosapi/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import com.Moreira.produtosapi.model.Produto;
 import com.Moreira.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -36,6 +37,28 @@ public class ProdutoController {
     @GetMapping("{id}")
     public Produto obterPorId(@PathVariable("id") String id) {
         return produtoRepository.findById(id).orElse(null);
+    }
+
+
+    @DeleteMapping("{id}")
+    //Deletar Produtos
+    public void deletar ( @PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+
+    }
+    @PutMapping("{id}")
+    // Atualizar o produto
+    public void atualizar (@PathVariable ("id") String id,@RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+        //Busca de Produto
+        //Criando um metodo no produtoRepository
+
+        public List <Produto> buscar(@RequestParam("nome")String nome){
+            return produtoRepository.findByNome(nome);
     }
 }
 
--- produtosapi/src/main/java/com/Moreira/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/com/Moreira/produtosapi/repository/ProdutoRepository.java
@@ -2,5 +2,9 @@ package com.Moreira.produtosapi.repository;
 import com.Moreira.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto,String> {
+
+    List<Produto> findByNome(String nome);
 }
